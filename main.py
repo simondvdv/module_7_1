0b3daaa8bb69343d14267db2ecd6bc63fda3b5c0
simondv4 <simondv4@gmail.com>
2024-07-27 22:56:41 +0300

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 
+
 class Product:
     def __init__(self, name, weight, category):
         self.name = name
@@ -16,17 +17,22 @@ class Shop:
 
     def get_products(self):
         products = open(self.__file_name, 'r')
-        print(products.read())
-        products.close()
+        product_string = products.read()
+        answer = ''
+        product_list = product_string.replace('\n', ', ').replace(' ', '').split(',')
+        for i in range(0, len(product_list), 3):
+            answer += product_list[i] + ' '
+        return answer
+
 
     def add(self, *args):
-        products = open(self.__file_name, 'a')
-        products_r = open(self.__file_name, 'r')
         for i in args:
-            print(' '.join(products_r.read().split('\n')))
-            if not (i.name in products_r.read()):
+            products = open(self.__file_name, 'a')
+            if not (i.name in Shop.get_products(self)):
                 products.write(f'{i.name}, {i.weight}, {i.category}\n')
-        products.close()
+            else:
+                print(f'Товар {i.name} уже есть в магазине')
+            products.close()
 
 
 s1 = Shop()
@@ -34,9 +40,10 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-
+print(p2)
 
 s1.add(p1, p2, p3)
 
+print(s1.get_products())
 
 
